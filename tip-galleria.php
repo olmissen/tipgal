c936284d19e2e345148cd9369aaf27a5c7616a49
olmissen <olmissen@olmissens-MacBook.local>
2012-09-28 12:56:08 +0200

    added js image array creator and lots of other stuff

--- tip-galleria.php
+++ tip-galleria.php
@@ -27,43 +27,11 @@ License: Properitary
     return $content;
   }
 ?><?php
-  function tip_init()
-  {
+function tip_init()
+{
 ?>
 <div id="galleria"></div>
-<div id="closeit"><img src="<?php echo (WP_PLUGIN_URL . "/tip-galleria/images/closegal.png"); ?>"/></div>
-<?php
-        $attachments = get_children( array('post_parent' => get_the_ID(), 'post_type' => 'attachment', 'post_mime_type' =>'image'));
-        class ImageInfo{
-                  public $image = "";
-                  public $thumb  = "";
-                }
-        $all_images = array();
-        foreach ( $attachments as $attachment_id => $attachment ) {
-        $json_image = new ImageInfo();
-        $image = wp_get_attachment_image($attachment_id, 'medium');
-        $dom = simplexml_load_string($image);
-        $image_url = $dom->attributes()->src;
-        $image_url = str_ireplace("http://grammofik.dk/", '', $image_url);
-        //$image_url = htmlspecialchars($image_url);
-        //assign thumbnail before regex removes thump suffix
-        $json_image->thumb  = $image_url;
-        //fjerner thumpnail trailing size this will prob break later!!!
-        $image_url = preg_replace("#(^.*)(\W[0-9]+\w[0-9]+)(.*)$#", "$1$3", $image_url);
-        echo $image_url;
-        echo "<br>";
-        //add real path ass img path
-        $json_image->image = $image_url;
-        // Returns: {"firstname":"foo","lastname":"bar"}
-        array_push($all_images, $json_image);
-        }
-    //debug info
-    //print_r($all_images);
-//the next script data thing transfers the jason data to javascript
-?>
-<script>
-var data = <?php echo json_encode($all_images);?>;
-</script>
+<div id="closeit"><img src="<?php echo (WP_PLUGIN_URL . "/tip-galleria/images/closegal2.png"); ?>"/></div>
 <?php
 }
 ?><?php
--- tip.galleria.js
+++ tip.galleria.js
@@ -1,20 +1,63 @@
 $(document).ready(function(){
+
+function gal_img(image,thumb)
+{
+    this.image=image;
+    this.thumb=thumb;
+}
+
+$.extend($.fn.disableTextSelect = function() {
+    return this.each(function(){
+        if($.browser.mozilla){//Firefox
+            $(this).css('MozUserSelect','none');
+        }else if($.browser.msie){//IE
+            $(this).bind('selectstart',function(){return false;});
+        }else{//Opera, etc.
+            $(this).mousedown(function(){return false;});
+        }
+    });
+});
+
+imgs = $(".gallery-item a").map(function() {
+  return $(this).attr("href");
+});
+img_thumbs = $("#main a img").map(function() {
+  return $(this).attr("src");
+});
+
+gInit = false;
+data = [];
+for(var i=0;i<imgs.length;i++){
+    data.push(new gal_img(imgs[i],img_thumbs[i]));
+}
     //initialise galleria
     Galleria.loadTheme('/wp-content/plugins/tip-galleria/galleria/themes/classic/galleria.classic.js');
     Galleria.run('#galleria', {
         dataSource: data,
         preload: 2,
         transition: 'fade',
-        transitionSpeed: 25,
+        transitionSpeed: 10,
         trueFullscreen: false,
         maxScaleRatio:1
         });
 
+    //$('#galleria').disableTextSelect();//No text selection on elements with a class of 'noSelect'
+    
     $(".gallery-item a").click(function(e){
         e.preventDefault();
+        g = Galleria.get(0);
+        //var msg = $(this).attr("href");
+        if (!gInit){
+            Galleria.configure({
+            show: jQuery.inArray($(this).attr("href"), imgs)
+            });
+            gInit = true;
+        }
+        else{
+            g.show(jQuery.inArray($(this).attr("href"), imgs));
+        }
+
         $("html").css("overflow", "hidden");
-        var g = Galleria.get(0);
-        g.resize();
         $('#galleria').fadeIn();
         $('#closeit').fadeTo(300, 0.90);
         var wWidth = $(window).width();
@@ -22,29 +65,34 @@ $(document).ready(function(){
         var dHeight = $(document).height();
         var gWidth = $('#galleria').width();
         var gHeight = $('#galleria').height();
-        //$('#overlay').width(gWidth);
-        //$('#overlay').height(gHeight);
-  /*    
-        $('#galleria').css('top', ((wHeight)/2)-((gHeight)/2)+'px');
-        $('#galleria').css('left', ((wWidth)/2)-((gWidth)/2)+'px');
-
-        $('#closeit').css('left', ((gWidth/2)+10)+'px');
-        $('#closeit').css('top', ((wHeight)/2)-((gHeight)/2)+'px');
-        $('#closeit').css('position', 'relative');
-
-        $('#galleria').fadeIn();
-    */    
 
         $("#close-galleria").click(function(e){
             $('#closeit').fadeOut();
             $('#galleria').fadeOut();
             $("html").css("overflow", "auto");
         });
+        
+        Galleria.on('image', function(e) {
+            $(e.imageTarget).click(function () {
+                g.next();
+            });
+        });
+        
 
         $("#closeit img").click(function(e){
             $('#closeit').fadeOut();
             $('#galleria').fadeOut();
             $("html").css("overflow", "auto");
+        });
+
+        $('#closeit').fadeTo('fast', 0.2);
+
+        $("#closeit").hover(
+            function() {
+                $(this).stop().animate({ opacity: 0.9 }, 400);
+            },
+            function() {
+                $(this).stop().animate({ opacity: 0.2 }, 800);
         });
 
         g.attachKeyboard({
