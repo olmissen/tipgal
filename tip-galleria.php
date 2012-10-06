952a8db9076647389253efa32b8982f148758a72
olmissen <olmissen@olmissens-MacBook.local>
2012-10-06 22:47:16 +0200

    placed galleria.run INSIDE the calling image click function...

--- tip-galleria.php
+++ tip-galleria.php
@@ -6,37 +6,28 @@ Description: Simple implementation of io-gallaria.
 Version: 0.6.0
 Author: Jeppe Fastrup
 Author URI: http://theinflatablepenguin.com
-License: Properitary
+License: Dont care
 */
 ?><?php
   function tip_scripts(){
     wp_deregister_script( 'jquery' );
     wp_register_script( 'jquery', 'http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js');
     wp_enqueue_script( 'jquery' );
-    wp_enqueue_style("tip_galleria_style",WP_PLUGIN_URL . "/tip-galleria/tip.galleria.css");
-    wp_enqueue_style("galleria_theme_style",WP_PLUGIN_URL . "/tip-galleria/galleria/themes/classic/galleria.classic.css");
-    wp_enqueue_script("galleria_theme_script",WP_PLUGIN_URL . "/tip-galleria/galleria/themes/classic/galleria.classic.min.js");
     wp_enqueue_script("galleria-io",WP_PLUGIN_URL . "/tip-galleria/galleria/galleria-1.2.8.min.js");
+    wp_enqueue_style("galleria_theme_style",WP_PLUGIN_URL . "/tip-galleria/galleria/themes/classic/galleria.classic.css");
     wp_enqueue_script("tip-galleria-wrapper", WP_PLUGIN_URL . "/tip-galleria/tip.galleria.js");
+    wp_enqueue_style("tip_galleria_style",WP_PLUGIN_URL . "/tip-galleria/tip.galleria.css");
   }
-?><?php
-  function tip_content($content)
-  {
-    echo '<!-- DEBUG STAMP tip_content() -->';
-
-    return $content;
-  }
-?><?php
+?>
+<?php
 function tip_init()
 {
 ?>
 <div id="galleria"></div>
-<div id="closeit"><img src="<?php echo (WP_PLUGIN_URL . "/tip-galleria/images/closegal2.png"); ?>"/></div>
+<div id="closeit"><img id="closeit-img" alt="Exit Gallery Button" src="<?php echo (WP_PLUGIN_URL . "/tip-galleria/images/closegal2.png"); ?>"/></div>
 <?php
 }
 ?><?php
-/*add_filter( 'gallery_style', 'tip_gallery_style', 99 );*/
-add_filter('the_content', 'tip_content', 99);
-add_action('wp_footer', 'tip_init',99);
+add_action('wp_footer', 'tip_init',500);
 add_action('wp_enqueue_scripts', 'tip_scripts',100);
 ?>
--- tip.galleria.js
+++ tip.galleria.js
@@ -1,11 +1,16 @@
 $(document).ready(function(){
-
+    console.log('tip.galleria.js START');
+//image object see the galleria specs for more properties.
 function gal_img(image,thumb)
 {
     this.image=image;
     this.thumb=thumb;
 }
 
+//custom settings:
+_IdleExitOnKeyboard = true;
+
+//supposedly removes the ability to select text and img.
 $.extend($.fn.disableTextSelect = function() {
     return this.each(function(){
         if($.browser.mozilla){//Firefox
@@ -18,96 +23,122 @@ $.extend($.fn.disableTextSelect = function() {
     });
 });
 
-imgs = $(".gallery-item a").map(function() {
+//variable false if gallery has not been initialized.
+gInit = false;
+
+//todo: This is maybe stupid - thumbs and img are paired without knowing wheather both exists and are actually related.
+imgs = $(".gallery-item a").has('img').map(function() {
   return $(this).attr("href");
 });
-img_thumbs = $("#main a img").map(function() {
+
+img_thumbs = $(".gallery-item a img").map(function() {
   return $(this).attr("src");
 });
-
-gInit = false;
+//data holds the image object array that is parsed to gallaria.
 data = [];
 for(var i=0;i<imgs.length;i++){
+    //console.log('Image: ' + imgs[i] + ' paired with thumb: ' + img_thumbs[i] + '.');
     data.push(new gal_img(imgs[i],img_thumbs[i]));
 }
-    //initialise galleria
-    Galleria.loadTheme('/wp-content/plugins/tip-galleria/galleria/themes/classic/galleria.classic.js');
-    Galleria.run('#galleria', {
-        dataSource: data,
-        preload: 2,
-        transition: 'fade',
-        transitionSpeed: 10,
-        trueFullscreen: false,
-        maxScaleRatio:1
-        });
 
-    //$('#galleria').disableTextSelect();//No text selection on elements with a class of 'noSelect'
+
+    console.log('galleria.run');
+
+    $('#galleria').disableTextSelect();//No text selection
     
     $(".gallery-item a").click(function(e){
         e.preventDefault();
-        g = Galleria.get(0);
-        //var msg = $(this).attr("href");
-        if (!gInit){
-            Galleria.configure({
-            show: jQuery.inArray($(this).attr("href"), imgs)
-            });
-            gInit = true;
-        }
-        else{
-            g.show(jQuery.inArray($(this).attr("href"), imgs));
-        }
+        //console.log('Image clicked');
+        //shows galleria and closeit button
+        //todo: add closeit button to galleria dom.
 
         $("html").css("overflow", "hidden");
         $('#galleria').fadeIn();
-        $('#closeit').fadeTo(300, 0.90);
-        var wWidth = $(window).width();
-        var wHeight = $(window).height();
-        var dHeight = $(document).height();
-        var gWidth = $('#galleria').width();
-        var gHeight = $('#galleria').height();
-
-        $("#close-galleria").click(function(e){
-            $('#closeit').fadeOut();
-            $('#galleria').fadeOut();
-            $("html").css("overflow", "auto");
-        });
-        
-        Galleria.on('image', function(e) {
-            $(e.imageTarget).click(function () {
-                g.next();
-            });
-        });
-        
-
-        $("#closeit img").click(function(e){
-            $('#closeit').fadeOut();
-            $('#galleria').fadeOut();
-            $("html").css("overflow", "auto");
-        });
-
-        $('#closeit').fadeTo('fast', 0.2);
-
-        $("#closeit").hover(
+        $('#wpadminbar').slideUp();
+        $('#closeit').fadeIn();
+        $('#closeit-img').fadeTo('fast', 0.2);
+        $("#closeit-img").hover(
             function() {
-                $(this).stop().animate({ opacity: 0.9 }, 400);
+                $("#closeit-img").stop().animate({ opacity: 0.9 }, 250);
             },
             function() {
-                $(this).stop().animate({ opacity: 0.2 }, 800);
+                $("#closeit-img").stop().animate({ opacity: 0.2 }, 1000);
         });
-
-        g.attachKeyboard({
-          escape: function() {
+        $("#closeit-img").click(function(e){
             $('#closeit').fadeOut();
             $('#galleria').fadeOut();
+            $('#wpadminbar').slideDown();
+            //makes scrollbar appear again if needed.
             $("html").css("overflow", "auto");
-          }
         });
-        g.attachKeyboard({
-          left: g.prev,
-          right: g.next
-        });
+        if (!gInit){
+            //initialise galleria
+            Galleria.loadTheme('/wp-content/plugins/tip-galleria/galleria/themes/classic/galleria.classic.js');
+            //console.log('themeLoad');
+            Galleria.run('#galleria', {
+                dataSource: data,
+                preload: 2,
+                transition: 'fade',
+                transitionSpeed: 10,
+                trueFullscreen: false,
+                maxScaleRatio:1,
+                idleTime:5000,
+                responsive: true,
+                extend: function() {
+                    //$("#closeit").appendTo("#galleria");
+                    }
+                });
+            g = Galleria.get(0);
+            Galleria.on('image', function(e) {
+                    $(e.imageTarget).click(function () {
+                        g.next();
+                    });
+                });
+            //console.log('try to show: ' + jQuery.inArray($(this).attr("href"), imgs));
+            Galleria.configure({
+                show: jQuery.inArray($(this).attr("href"), imgs)
+                });
+
+            g.addIdleState('#closeit',{opacity:0});
+            g.addIdleState(g.get('thumbnails-container'),{opacity:0});
+            g.attachKeyboard({
+                37: function() {
+                    if (_IdleExitOnKeyboard){
+                        g.$('image-nav-left').click();
+                    }
+                    else{
+                        g.prev();
+                    }            
+                },
+                39: function() {
+                    if (_IdleExitOnKeyboard){
+                        g.$('image-nav-right').click();
+                    }
+                    else{
+                        g.next();
+                    }
+                },                
+                escape: function() {
+                  $('#closeit-img').click();
+                },
+                32: function() { //keycode 32 is spacebar. g.next doesnt trigger idleexit.
+                    g.next();
+                }
+                });
+                gInit = true;
+        }
+        else{
+            g = Galleria.get(0);
+            //console.log('try to show: ' + jQuery.inArray($(this).attr("href"), imgs));
+            g.show(jQuery.inArray($(this).attr("href"), imgs));
+        }
+
+
+
+
+
         $(window).resize(function() { // window resized
-            g.resize();
+            //g.resize();
         });
 
     });
